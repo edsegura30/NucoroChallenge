439dc7ef4aba3547f0f969acb5a399ee7fdb587d
Edgar Segura <edsegura1608@gmail.com>
2022-06-18 22:03:23 -0500

    connects proyect to PSQL DB

--- currency_exchanger/settings.py
+++ currency_exchanger/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'currency_exchanger.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': os.environ.get('DJANGO_DB_HOST'),
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
 
